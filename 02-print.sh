395c5aa1fc79ae86fe007cd7b88a0f4b79b67e5c
bagnedinesh <dineshbagne@gmail.com>
2023-01-05 14:18:55 +0530

    shell practice

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,10 @@ echo 'All Special chars - !@#$%^&*()_+/?><'
 echo "All Special chars - !@#$%^&*()_+/?><"
 
 echo $Dinesh !@#$%^&*()_+/?><
+
+echo -e "\e[31mMESSAGE\e[0m" # Red
+echo -e "\e[32mMESSAGE\e[0m" # Green
+echo -e "\e[33mMESSAGE\e[0m" # Yellow
+echo -e "\e[34mMESSAGE\e[0m" # Blue
+echo -e "\e[35mMESSAGE\e[0m" # Magenta
+echo -e "\e[36mMESSAGE\e[0m" # Cyan
