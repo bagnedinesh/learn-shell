5454e4d6fd5d1d162d2d898c5da91085efbc291f
bagnedinesh <dineshbagne@gmail.com>
2023-01-05 16:31:29 +0530

    shell practice

--- 02-print.sh
+++ 02-print.sh
@@ -6,8 +6,8 @@ echo "All Special chars - !@#$%^&*()_+/?><"
 
 #echo $Dinesh !@#$%^&*()_+/?><
 
-echo -e "\e[31mMESSAGE\e[0m" # Red
-echo -e "\e[32mMESSAGE\e[0m" # Green
+echo -e "\e[31mMESSAGE\e[0m" Dinesh # Red
+echo -e "\e[32mMESSAGE\e[0m" Bagne # Green
 echo -e "\e[33mMESSAGE\e[0m" # Yellow
 echo -e "\e[34mMESSAGE\e[0m" # Blue
 echo -e "\e[35mMESSAGE\e[0m" # Magenta
