cb740685c88167cfbd9d42f54178480987176083
bagnedinesh <dineshbagne@gmail.com>
2023-01-10 07:11:15 +0530

    updating ./learn-shell

---
+++ function.sh
@@ -0,0 +1,8 @@
+#functions are like variable
+#variable - when assign name to a set of data then its variable
+#Function - when assign a name to set of commands then its function
+
+function foo() {
+    echo this is function example
+}
+foo
