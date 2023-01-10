f8d8476883067c41319d58021c96d8984d3fa2d7
bagnedinesh <dineshbagne@gmail.com>
2023-01-10 07:40:57 +0530

    updating ./learn-shell

---
+++ 06-special-vars.sh
@@ -0,0 +1,19 @@
+#following values from script
+echo "0 - $0" #script file name
+echo "1 - $1" #first org value
+echo "2 - $2" #second org value
+echo "3 - $3" #third org value
+echo "* - $*" #all the arg values
+echo "@ - $@" #all the org values
+echo "# - $#" #Number of org passed 
+
+#Following values from function
+example(){
+  echo "0 - $0"
+  echo "1 - $1"
+  echo "2 - $2"
+  echo "3 - $3"
+  echo "* - $*"
+  echo "@ - $@"
+  echo "# - $#"
+}
